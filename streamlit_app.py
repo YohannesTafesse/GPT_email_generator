048a68570283cdecc17774f2e44291396dfe0cd2
YohannesTafesse <yz.tafesse@gmail.com>
2024-03-18 23:14:40 +0000

    Made significant updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,22 @@
 # gpt3 professional email generator by stefanrmmr - version June 2022
 
 import os
-import openai
+#import openai
 import streamlit as st
 
+
+#added by Yohannes
+import transformers
+import torch
+
+from transformers.utils import logging
+logging.set_verbosity_error()
+
+from transformers import pipeline 
+
+
 # DESIGN implement changes to the standard streamlit UI/UX
-st.set_page_config(page_title="rephraise", page_icon="img/rephraise_logo.png",)
+st.set_page_config(page_title="Amharic - English Translator", page_icon="img/rephraise_logo.png",)
 # Design move app further up and remove top padding
 st.markdown('''<style>.css-1egvi7u {margin-top: -4rem;}</style>''',
     unsafe_allow_html=True)
@@ -33,108 +44,67 @@ st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
 
 
 # Connect to OpenAI GPT-3, fetch API key from Streamlit secrets
-openai.api_key = os.getenv("OPENAI_API_KEY")
+#openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def gen_mail_contents(email_contents):
+def translate (input_text, source_language, destination_language):
 
     # iterate through all seperate topics
-    for topic in range(len(email_contents)):
-        input_text = email_contents[topic]
-        rephrased_content = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=f"Rewrite the text to be elaborate and polite.\nAbbreviations need to be replaced.\nText: {input_text}\nRewritten text:",
-            # prompt=f"Rewrite the text to sound professional, elaborate and polite.\nText: {input_text}\nRewritten text:",
-            temperature=0.8,
-            max_tokens=len(input_text)*3,
-            top_p=0.8,
-            best_of=2,
-            frequency_penalty=0.0,
-            presence_penalty=0.0)
+    #for topic in range(len(contents)):
+        #input_text = contents[input_text]
+        #source_language = contents[source_language]
+        #destination_language = contents[destination_language]
 
-        # replace existing topic text with updated
-        email_contents[topic] = rephrased_content.get("choices")[0]['text']
-    return email_contents
+    translator = pipeline(task="translation",
+                      model="facebook/nllb-200-distilled-600M",
+                      torch_dtype=torch.bfloat16)
 
-
-def gen_mail_format(sender, recipient, style, email_contents):
-    # update the contents data with more formal statements
-    email_contents = gen_mail_contents(email_contents)
-    # st.write(email_contents)  # view augmented contents
-
-    contents_str, contents_length = "", 0
-    for topic in range(len(email_contents)):  # aggregate all contents into one
-        contents_str = contents_str + f"\nContent{topic+1}: " + email_contents[topic]
-        contents_length += len(email_contents[topic])  # calc total chars
-
-    email_final_text = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Write a professional email sounds {style} and includes Content1 and Content2 in that order.\n\nSender: {sender}\nRecipient: {recipient} {contents_str}\n\nEmail Text:",
-        # prompt=f"Write a professional sounding email text that includes all of the following contents separately.\nThe text needs to be written to adhere to the specified writing styles and abbreviations need to be replaced.\n\nSender: {sender}\nRecipient: {recipient} {contents_str}\nWriting Styles: motivated, formal\n\nEmail Text:",
-        temperature=0.8,
-        max_tokens=contents_length*2,
-        top_p=0.8,
-        best_of=2,
-        frequency_penalty=0.0,
-        presence_penalty=0.0)
-
-    return email_final_text.get("choices")[0]['text']
-
+    if (source_language == 'Amahric'):
+            source_language = 'amh_Ethi'
+            destination_language = 'eng_Latn'
+    else:
+            source_language = 'eng_Latn'
+            destination_language = 'amh_Ethi' 
+                      
+    text_translated = translator(input_text,
+                             src_lang = source_language,
+                             tgt_lang = destination_language)    
+    return text_translated
 
 def main_gpt3emailgen():
 
     st.image('img/image_banner.png')  # TITLE and Creator information
-    st.markdown('Generate professional sounding emails based on your direct comments - powered by Artificial Intelligence (OpenAI GPT-3) Implemented by '
+    st.markdown('Translate Amharic to English or English to Amharic based on your direct comments - powered by Artificial Intelligence (OpenAI GPT-3) Implemented by '
         '[stefanrmmr](https://www.linkedin.com/in/stefanrmmr/) - '
         'view project source code on '
         '[GitHub](https://github.com/stefanrmmr/gpt3_email_generator)')
     st.write('\n')  # add spacing
 
-    st.subheader('\nWhat is your email all about?\n')
-    with st.expander("SECTION - Email Input", expanded=True):
+   # st.subheader('\nWhat is your email all about?\n')
+    
+    with st.expander("SECTION - Lnguage Input", expanded=True):
 
-        input_c1 = st.text_input('Enter email contents down below! (currently 2x seperate topics supported)', 'topic 1')
-        input_c2 = st.text_input('', 'topic 2 (optional)')
-
-        email_text = ""  # initialize columns variables
-        col1, col2, col3, space, col4 = st.columns([5, 5, 5, 0.5, 5])
-        with col1:
-            input_sender = st.text_input('Sender Name', '[rephraise]')
-        with col2:
-            input_recipient = st.text_input('Recipient Name', '[recipient]')
-        with col3:
-            input_style = st.selectbox('Writing Style',
-                                       ('formal', 'motivated', 'concerned', 'disappointed'),
-                                       index=0)
-        with col4:
-            st.write("\n")  # add spacing
-            st.write("\n")  # add spacing
-            if st.button('Generate Email'):
-                with st.spinner():
-
-                    input_contents = []  # let the user input all the data
-                    if (input_c1 != "") and (input_c1 != 'topic 1'):
-                        input_contents.append(str(input_c1))
-                    if (input_c2 != "") and (input_c2 != 'topic 2 (optional)'):
-                        input_contents.append(str(input_c2))
-
-                    if (len(input_contents) == 0):  # remind user to provide data
-                        st.write('Please fill in some contents for your message!')
-                    if (len(input_sender) == 0) or (len(input_recipient) == 0):
-                        st.write('Sender and Recipient names can not be empty!')
-
-                    if (len(input_contents) >= 1):  # initiate gpt3 mail gen process
-                        if (len(input_sender) != 0) and (len(input_recipient) != 0):
-                            email_text = gen_mail_format(input_sender,
-                                                         input_recipient,
-                                                         input_style,
-                                                         input_contents)
-    if email_text != "":
-        st.write('\n')  # add spacing
-        st.subheader('\nYou sound incredibly professional!\n')
-        with st.expander("SECTION - Email Output", expanded=True):
-            st.markdown(email_text)  #output the results
-
+        source_language = st.selectbox('Source Language', ('Amahric', 'English'), index=0)
+        input_text = st.text_input('Enter message to be transalted below', 'message')
+       
+        input_contents = []  # input data vector
+        
+        #Determine the destination language
+        if (source_language == 'Amahric'):
+              destination_language = 'English'
+        else:
+              destination_language = 'Amharic'
+        
+        if (input_text != ""):
+            input_contents.append(str(input_text)) #input text to be translation
+            input_contents.append(str(source_language)) #source language
+            input_contents.append(str(destination_language)) #destination language
+      
+        
+    if (len(input_contents) >= 1):  # initiate gpt3 mail gen process
+                        #if (len(input_sender) != 0):
+                        translated_text = translate(input_text, source_language, destination_language)
+                        #st.markdown(translated_text)
 
 if __name__ == '__main__':
     # call main function
